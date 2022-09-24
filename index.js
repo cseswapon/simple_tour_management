6c93f04024747d8206087fadbf1024a5bae8dd0c
cseswapon <cseswapon@gmail.com>
2022-09-24 09:19:59 +0600

    [ADD] - simple file change

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const tourProduct = require("./routes/v1/tour.route");
 const app = express();
-
+const mongoose = require("mongoose");
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use('/api/v1/tour',tourProduct)
+const port = process.env.PORT || 5000;
 
 // root route
 app.get("/", (req, res) => {
@@ -16,6 +18,17 @@ app.get("/", (req, res) => {
 // by default route
 app.get("*", (req, res) => {
   res.status(404).send({ message: "Route Not Found" });
+});
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Database Connected Successfully");
+  })
+  .catch((error) => console.log(error));
+
+app.listen(port, () => {
+  console.log("Server Side Running is", port);
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = require("./index");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 
+
 mongoose
   .connect(process.env.DB_URL)
   .then(() => {
